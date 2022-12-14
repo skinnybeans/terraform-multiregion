08e5ed40fd9b3a977f6d24f245df93c9f598864f
Henrik Axelsson <skinnybeans@gmail.com>
2022-12-14 18:35:15 +1100

    fixed subnet AZ

--- test/ap-southeast-2/vpc/main.tf
+++ test/ap-southeast-2/vpc/main.tf
@@ -25,7 +25,7 @@ module "vpc" {
   name = "my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-1b", "us-west-1c"]
+  azs             = ["ap-southeast-2a", "ap-southeast-2b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
@@ -33,7 +33,7 @@ module "vpc" {
   enable_vpn_gateway = false
 
   tags = {
-    Terraform   = "true"
+    ManagedBy   = "terraform"
     Environment = "dev"
   }
 }
